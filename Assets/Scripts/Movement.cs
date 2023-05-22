6de00ba831183364659c89d89df0285e28c24897
dmaheshwari2209 <100116359+dmaheshwari2209@users.noreply.github.com>
2023-05-22 19:27:54 +0530

    rocket movement
    sample obstacle, thrust, rotation and frame independent movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Rigidbody rb;
+
+    [SerializeField] float mainThrust = 1f;
+    [SerializeField] float rotationThrust = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+        }
+    }
+
+    void ProcessRotation()
+    {
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            ApplyRotation(rotationThrust);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            ApplyRotation(-rotationThrust);
+        }
+    }
+
+    void ApplyRotation(float rotationFactor)
+    {
+        rb.freezeRotation = true;
+        transform.Rotate(Vector3.forward * rotationFactor * Time.deltaTime);
+        rb.freezeRotation = false;
+    }
+}
