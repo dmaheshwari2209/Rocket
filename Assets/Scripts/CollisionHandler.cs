4844ac4b42c8662d9785e3a4e1e17e148e37a346
dmaheshwari2209 <100116359+dmaheshwari2209@users.noreply.github.com>
2023-05-22 21:31:58 +0530

    respawing on collision

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionHandler : MonoBehaviour
+{
+    void OnCollisionEnter(Collision other)
+    {
+        switch (other.gameObject.tag)
+        {
+            case "Launch":
+            case "Finish":
+                break;
+            default:
+                ReloadLevel();
+                break;
+        }
+    }
+
+    void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+}
